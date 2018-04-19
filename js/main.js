b25d0e49c4df194db05324b7ff03449e7312e18a
MiaCarulla <mia.carulla@yahoo.com>
2018-04-19 15:13:27 +0100

    Pretty cool css changes
    Nearly there with the whole page.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,55 @@
-	//menu section
+//scroll page 
+$(function() {
+	$("nav a").click(function() {
+		var element = $(this).attr("href");
 
-	(function() {
-			[].slice.call(document.querySelectorAll('.menu')).forEach(function(menu) {
-				var menuItems = menu.querySelectorAll('.menu__link'),
-					setCurrent = function(ev) {
-						ev.preventDefault();
-						var item = ev.target.parentNode; // li
-						// return if already current
-						if (classie.has(item, 'menu__item--current')) {
-							return false;
-						}
-						// remove current
-						classie.remove(menu.querySelector('.menu__item--current'), 'menu__item--current');
-						// set current
-						classie.add(item, 'menu__item--current');
-					};
-				[].slice.call(menuItems).forEach(function(el) {
-					el.addEventListener('click', setCurrent);
-				});
-			});
-			[].slice.call(document.querySelectorAll('.link-copy')).forEach(function(link) {
-				link.setAttribute('data-clipboard-text', location.protocol + '//' + location.host + location.pathname + '#' + link.parentNode.id);
-				new Clipboard(link);
-				link.addEventListener('click', function() {
-					classie.add(link, 'link-copy--animate');
-					setTimeout(function() {
-						classie.remove(link, 'link-copy--animate');
-					}, 300);
-				});
-			});
-		})(window);
+	    $('html, body').animate({
+	        scrollTop: $(element).offset().top - 42
+	    }, 1000);
+	});
+});
+
+//FORM JAVA
+$(function() {
+  var floatingLabel;
+
+  floatingLabel = class floatingLabel {
+    constructor(form, options = {}) {
+      var event, i, input, j, label, len, len1, ref, ref1;
+      if (!form) {
+        return;
+      }
+      options.focusClass || (options.focusClass = "focus");
+      options.activeClass || (options.activeClass = "active");
+      options.errorClass || (options.errorClass = "error");
+      form.classList.add('has-floated-label');
+      ref = form.querySelectorAll('label');
+      for (i = 0, len = ref.length; i < len; i++) {
+        label = ref[i];
+        if (!(input = document.querySelector(`#${label.getAttribute('for')}`))) {
+          return;
+        }
+        ref1 = ['keyup', 'input', 'change'];
+        for (j = 0, len1 = ref1.length; j < len1; j++) {
+          event = ref1[j];
+          input.addEventListener(event, function() {
+            this.parentNode.classList.remove(options.errorClass);
+            return this.parentNode.classList.toggle(options.activeClass, !!this.value);
+          });
+        }
+        input.addEventListener('focus', function() {
+          return this.parentNode.classList.add(options.focusClass);
+        });
+        input.addEventListener('blur', function() {
+          return this.parentNode.classList.remove(options.focusClass);
+        });
+        input.parentNode.classList.toggle(options.activeClass, !!input.value);
+      }
+    }
+
+  };
+
+  // initialize
+  window.floatingLabel = new floatingLabel(document.querySelector('.form'));
+
+}).call(this);
